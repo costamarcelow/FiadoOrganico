734015087563147c37ba6b49cc6163367dfa1f62
Marcelo <Marcelo@DESKTOP-7H6V7LN>
2021-04-12 21:43:39 -0300

    aula 05

--- src/fiado/Main.java
+++ src/fiado/Main.java
@@ -1,58 +1,62 @@
 package fiado;
 
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		  int[] fiados = new int[10];
-		//fiados[0] = 12;
-		//fiados[3] = 87;
-
-		
-		
+		Consumidor consumidor = new Consumidor();
+		consumidor.fiados = new int[10];
+	
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Informa o nome do cliente: ");
-		String nome = scanner.nextLine();
-		
+		System.out.println("informe o nome do cliente: ");
+		consumidor.nome = scanner.nextLine();
 		System.out.println("Informe o valor da compra: ");
-		fiados[0] = Integer.valueOf(scanner.nextLine());
-		
-		int total = somaWhile(fiados);
-		
-		System.out.println("Cliente " + nome + " deve:  " + total);
+		consumidor.fiados[0] = Integer.valueOf(scanner.nextLine());
+
+		int total = somaWhile(consumidor.fiados);
+
+		if (total > 100) {
+
+			System.out.println("Cliente " + consumidor.nome + " deve " + total + " ganhou brinde");
+
+		} else {
+			System.out.println("Cliente " + consumidor.nome + " deve " + total);
+
+		}
 
 	}
+
 	public static int somaWhile(int[] fiados) {
 		int i = 0;
 		int total = 0;
-
 		while (i < fiados.length) {
 			total += fiados[i];
 			i++;
 
 		}
 		return total;
+
 	}
+
 	public static int somaFor(int[] fiados) {
 		int total = 0;
 		for (int i = 0; i < fiados.length; i++) {
 			total += fiados[i];
-			
+
 		}
 		return total;
+
 	}
-	
-	public static int somaForEach(int[] fiados) {
+
+	public static int somaForeach(int[] fiados) {
 		int total = 0;
 		for (int fiado : fiados) {
 			total += fiado;
-			
+
 		}
 		return total;
-	}
 
+	}
 }
